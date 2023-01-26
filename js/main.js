bfa180636b22694f8c7a644551b7b3e170f1095d
Deiny Plata <deinyplata.04@gmail.com>
2023-01-26 14:08:55 -0600

    Inicio de trabajo en la estructura del main con algunas clases

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
 import '../assets/styles/style.scss';
 
-document.querySelector('#app').innerHTML = `
-  <div>
- <p>Esta es mi plantilla para vanilla js hecha con vite </p>
-  </div>
-`;
+const navLinks = document.querySelectorAll(".nav-link li");
+const menuBtn = document.querySelector(".mobile-menu btn");
+const mobileMenu = document.querySelector("mobile-menu");
+const overFlow = document.querySelector("overflow");
+const movileLinks = document.querySelectorAll(".mobile-link li");
+
+
+menuBtn.addEventListener("click", () => {
+  const icon = document.querySelector(".mobile-menu-btn img");
+  menuBtn.classList.toggle("active-btn");
+  mobileMenu.classList.toggle("active-menu");
+  overFlow.classList.toggle("active menu");
+  if (menuBtn.classList.contains("active-btn")) {
+    icon.src = "./assets/imagenes/icon-close-menu.svg";
+  } else {
+    icon.src = "./assets/imagenes/icon-menu.svg";
+  }
+
+});
+
+
+navLinks.forEach ((link) => {
+  const subMenu = link.querySelector(".sub-menu");
+  link.addEventListener("click", () => {
+    link.classList.toggle("active");
+    subMenu = classList.toggle("active-menu");
+  })
+
+})
