1041a0d2c4af665e0a67431b6034b3eb4527aed0
unknown <deinyplata.04@gmail.com>
2023-01-30 13:01:58 -0600

    implementar main.js, en donde se crean const con la finalidad de dar funcionalidad al menu, en este espacio se desplaza el menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 import '../assets/styles/style.scss';
 
+const navLinks = document.querySelectorAll(".nav-links li");
+const menuBtn = document.querySelector(".mobile-menu-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+const overFlow = document.querySelector(".overflow");
+const mobileLinks = document.querySelectorAll(".mobile-links li");
+
+
+menuBtn.addEventListener("click", () => {
+  const icon = document.querySelector(".mobile-menu-btn img");
+  menuBtn.classList.toggle("active-btn");
+  mobileMenu.classList.toggle("active-menu");
+  overFlow.classList.toggle("active menu");
+  if (menuBtn.classList.contains("active-btn")) {
+    icon.src = "./assets/imagenes/icon-close-menu.svg";
+  } else {
+    icon.src = "./assets/imagenes/icon-menu.svg";
+  }
+
+});
+
+
+
+
 
